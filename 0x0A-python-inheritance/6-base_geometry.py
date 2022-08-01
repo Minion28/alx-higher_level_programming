15cb6979567bb11b50f8a7c9b08cf7ca0cc1226b
Kelvin Mwanzia <81253558+Minion28@users.noreply.github.com>
2022-08-01 21:52:23 +0300

    tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Improve Geometry.
+
+class BaseGeometry:
+    #An empty class
+
+    def area(self):
+        """Raises an exception
+        """
+        raise Exception("area() is not implemented")
