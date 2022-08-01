de76441f3f5a74e242ee1d6253bc914393edec2c
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-08-01 11:51:01 -0700

    tasks

--- 0x00-python-hello_world/0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print(f"{number} positive")
-elif nummber == 0:
-    print(f"{number} is Zero")
-else:
-    print(f"{number} is negative")
