34aed5f942d316d15e0ceda5f1314b33c11461ac
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-08-01 12:06:19 -0700

    tasks

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-#returns the list attributes and methods of an object
-def lookup(obj):
-
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Geometry module."""
-
-
-class BaseGeometry:
-    """An empty class"""
-    pass
