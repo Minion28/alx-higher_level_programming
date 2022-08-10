4a71ef781dd86e6b3a627125c8cc41af8f5ede76
Kelvin Mwanzia <81253558+Minion28@users.noreply.github.com>
2022-08-10 17:39:51 +0300

    tasks

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
-"""Test case for base class"""
+"""Test case for the base class"""
 
 
 class TestBase(unittest.TestCase):
-    """Tests base model"""
+    """Tests the base model"""
 
     def test_valid_id(self):
-        """Tests for valid id"""
+        """Tests for a valid id"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
     def test_is_valid_string(self):
-        """Testing string inputs"""
+        """Testing fo string inputs"""
         b2 = Base("ALX")
         self.assertEqual(b2.id, "ALX")
 
     def test_is_valid_multiple(self):
-        """Tests valid multiple"""
+        """Tests for valid multiple"""
         pass
 
 
