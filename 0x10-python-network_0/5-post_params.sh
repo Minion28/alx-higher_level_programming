ddeae5c9184a37ddb0c9aec892cccbd4fc214f62
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2022-10-22 00:39:16 +0300

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in URL, sends POST request to the passed URL, and displays body of the response.
-curl -sL "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
