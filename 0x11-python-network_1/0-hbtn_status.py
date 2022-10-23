9f2523923f71177b9219f0ad73ccc7eb07df1f69
Minion28 <wambuakelvin14@gmail.com>
2022-10-23 08:00:32 -0700

    what is my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''fetch https://alx-intranet.hbtn.io/status'''
-import urllib.request
 if __name__ == '__main__':
-    with urllib.request.urlopen('http"//alx-intranet.hbtn.io/status') as response:
+    import urllib.request
+    with urllib.request.urlopen('https"://alx-intranet.hbtn.io/status') as response:
         html = response.read
         print('Body response:')
-        print('\t- type:{}'.format(type(content)))
-        print('\t- content:{}'.format(type(content)))
-        print('\t- utf8 content:{}'.format(content.decode('utf8')))
+        print('\t- type:{}'.format(type(html)))
+        print('\t- content:{}'.format(type(html)))
+        print('\t- utf8 content:{}'.format(html.decode('utf8')))
